3c5671b8300daffdcad63084ef135e80d341dc24
lrbridge <lrbridge@users.noreply.github.com>
2014-03-01 21:23:28 -0800

    more draggable/droppable work… first directives

--- app/js/create-note.js
+++ app/js/create-note.js
@@ -3,6 +3,12 @@
 angular.module('snapnote').controller('CreateNoteCtrl',
     function($scope) {
 
-
+        $scope.options = [
+            {text: "a"},
+            {text: "ab"},
+            {text: "abc"}
+        ];
+        
+        $( ".draggable" ).draggable({ revert: true, helper: "clone" });
     });
 	
--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,37 @@
 
 /* Directives */
 
+angular.module('snapnote')
+    .directive('textbox', function() {
+        return {
+            restrict: 'E',
+            template: "<div draggable class='ui-widget-content col-lg-4 dragme'>"+
+                "<p>Drag me around</p>"+
+                "<input class='form-control input-lg' type='text' id='inputLarge'></div>"
+        };
+    });
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('snapnote')
+    .directive('draggable', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elm) {
+                elm.draggable();
+            }
+        };
+    });
+
+angular.module('snapnote')
+    .directive('droppable', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elm) {
+                elm.droppable({
+                    drop: function( event, ui ) {
+                        $( this ).addClass( "ui-state-highlight" ).find( "p" ).html( "Dropped!" );
+                        console.log("yay dropped!");
+                    }
+                });
+            }
+        };
+    });
